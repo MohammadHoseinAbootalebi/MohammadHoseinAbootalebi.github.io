89836f3e953c35aa1def87625982715c31cef14d
Ghaem313Ghaem <mohammad.ali.alavi.313@gmail.com>
2024-04-30 06:23:27 +0330

    .

--- assets/js/Main Screen - First Page/Main-Intro-Adaptive.js
+++ assets/js/Main Screen - First Page/Main-Intro-Adaptive.js
@@ -25,7 +25,7 @@ var downloadMyResumeTranslations = {
     },
     '한국인': {
         'text': '내 이력서 다운로드',
-        'fontFamily': "'Black Han Sans', serif",
+        'fontFamily': "'Black Han Sans'",
     },
     'Español': {
         'text': 'Descargar mi currículum',
@@ -49,7 +49,114 @@ var downloadMyResumeTranslations = {
     },
     'فارسی': {
         'text': 'دانلود رزومه من',
-        'fontFamily': "'A Titraj 1'",
+        'fontFamily': "'Lalezar', serif",
+    },
+};
+
+var longTextParagraphsIntros = {
+    // English
+    'English': {
+        'MainIntroFirstViewIntroParagraph': "I'm a self-taught and self-trainer who doesn't limit himself by just degrees or licenses. I learned in every second and every moment of my life to better who I were.",
+        'SecondViewRESPONSIBLETextParagraph': "I'm responsible for what I'm doing.",
+        'SecondViewONTIMETextParagraph': "I always try my best to do things at the right time.",
+        'SecondViewCREATIVETextParagraph': "I'm a new-methods searcher.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // German
+    'Deutsch': {
+        'MainIntroFirstViewIntroParagraph': "Ich bin Autodidakt und Selbsttrainer, der sich nicht auf bloße Abschlüsse oder Lizenzen beschränkt. Ich habe in jeder Sekunde und in jedem Moment meines Lebens gelernt, besser zu werden, wer ich war.",
+        'SecondViewRESPONSIBLETextParagraph': "Ich bin verantwortlich für das, was ich tue.",
+        'SecondViewONTIMETextParagraph': "Ich versuche immer mein Bestes, die Dinge zum richtigen Zeitpunkt zu erledigen.",
+        'SecondViewCREATIVETextParagraph': "Ich bin ein Sucher nach neuen Methoden.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // Chinese
+    '中国人': {
+        'MainIntroFirstViewIntroParagraph': "我是一个自学成才、自我训练的人，不会仅仅通过学位或执照来限制自己。 我在生命中的每一秒、每一刻都在学习如何更好地做自己。",
+        'SecondViewRESPONSIBLETextParagraph': "我对我所做的事负责。",
+        'SecondViewONTIMETextParagraph': "我总是尽力在正确的时间做事。",
+        'SecondViewCREATIVETextParagraph': "我是一个新方法的探索者。",
+        'fontFamily': "'Ma Shan Zheng', serif",
+    },
+    // Russian
+    'Русский': {
+        'MainIntroFirstViewIntroParagraph': "Я самоучка и самоучитель, который не ограничивает себя только степенями или лицензиями. Каждую секунду и каждый момент своей жизни я учился становиться лучше, кем я был.",
+        'SecondViewRESPONSIBLETextParagraph': "Я несу ответственность за то, что делаю.",
+        'SecondViewONTIMETextParagraph': "Я всегда стараюсь сделать все в нужное время.",
+        'SecondViewCREATIVETextParagraph': "Я ищу новые методы.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // French
+    'Français': {
+        'MainIntroFirstViewIntroParagraph': "Je suis un autodidacte et autodidacte qui ne se limite pas à de simples diplômes ou licences. J'ai appris à chaque seconde et à chaque instant de ma vie pour mieux qui j'étais.",
+        'SecondViewRESPONSIBLETextParagraph': "Je suis responsable de ce que je fais.",
+        'SecondViewONTIMETextParagraph': "Je fais toujours de mon mieux pour faire les choses au bon moment.",
+        'SecondViewCREATIVETextParagraph': "Je suis un chercheur de nouvelles méthodes.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // Japanese
+    '日本語': {
+        'MainIntroFirstViewIntroParagraph': "私は学位やライセンスだけで自分を制限しない、独学でセルフトレーナーです。 私は人生のあらゆる瞬間、そしてあらゆる瞬間に、自分が誰であるかをより良く学ぶことができました。",
+        'SecondViewRESPONSIBLETextParagraph': "私は自分のやっていることに対して責任があります。",
+        'SecondViewONTIMETextParagraph': "私は常に適切なタイミングで物事を行うよう最善を尽くしています。",
+        'SecondViewCREATIVETextParagraph': "私は新しい方法の探求者です。",
+        'fontFamily': "'Hina Mincho', serif",
+    },
+    // Korean
+    '한국인': {
+        'MainIntroFirstViewIntroParagraph': "저는 학위나 자격증만으로 자신을 제한하지 않고 독학하고 스스로 훈련하는 사람입니다. 나는 내 인생의 매 순간, 매 순간마다 내가 누구인지 더 나은 사람이 되는 법을 배웠습니다.",
+        'SecondViewRESPONSIBLETextParagraph': "나는 내가 하는 일에 책임이 있다.",
+        'SecondViewONTIMETextParagraph': "나는 항상 적절한 시기에 일을 처리하기 위해 최선을 다합니다.",
+        'SecondViewCREATIVETextParagraph': "나는 새로운 방법을 찾는 사람입니다.",
+        'fontFamily': "'Gowun Batang', serif",
+    },
+    // Spanish
+    'Español': {
+        'MainIntroFirstViewIntroParagraph': "Soy un autodidacta y autoformador que no se limita sólo a títulos o licencias. Aprendí en cada segundo y en cada momento de mi vida a mejorar quién era.",
+        'SecondViewRESPONSIBLETextParagraph': "Soy responsable de lo que estoy haciendo.",
+        'SecondViewONTIMETextParagraph': "Siempre hago lo mejor que puedo para hacer las cosas en el momento adecuado.",
+        'SecondViewCREATIVETextParagraph': "Soy un buscador de nuevos métodos.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // Hindi
+    'हिंदी': {
+        'MainIntroFirstViewIntroParagraph': "मैं एक स्व-सिखाया हुआ और स्व-प्रशिक्षक हूं जो खुद को सिर्फ डिग्री या लाइसेंस तक सीमित नहीं रखता। मैंने अपने जीवन के हर सेकंड और हर पल में सीखा कि मैं जो हूं, उसे बेहतर बनाऊं।",
+        'SecondViewRESPONSIBLETextParagraph': "मैं जो कर रहा हूं उसके लिए मैं जिम्मेदार हूं।",
+        'SecondViewONTIMETextParagraph': "मैं हमेशा चीजों को सही समय पर करने की पूरी कोशिश करता हूं।",
+        'SecondViewCREATIVETextParagraph': "मैं नये तरीकों का खोजकर्ता हूं।",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // Portuguese
+    'Português': {
+        'MainIntroFirstViewIntroParagraph': "Sou um autodidata e autotreinador que não se limita apenas a diplomas ou licenças. Aprendi a cada segundo e a cada momento da minha vida a melhorar quem eu era.",
+        'SecondViewRESPONSIBLETextParagraph': "Sou responsável pelo que estou fazendo.",
+        'SecondViewONTIMETextParagraph': "Eu sempre tento o meu melhor para fazer as coisas na hora certa.",
+        'SecondViewCREATIVETextParagraph': "Sou um pesquisador de novos métodos.",
+        'fontFamily': "'Roboto Condensed', sans-serif",
+    },
+    // Bengali
+    'বাংলা': {
+        'MainIntroFirstViewIntroParagraph': "আমি একজন স্ব-শিক্ষিত এবং স্ব-প্রশিক্ষক যিনি নিজেকে শুধুমাত্র ডিগ্রী বা লাইসেন্স দ্বারা সীমাবদ্ধ করেন না। আমি আমার জীবনের প্রতিটি সেকেন্ডে এবং প্রতিটি মুহুর্তে শিখেছি যে আমি কে ছিলাম।",
+        'SecondViewRESPONSIBLETextParagraph': "আমি যা করছি তার জন্য আমি দায়ী।",
+        'SecondViewONTIMETextParagraph': "আমি সব সময়ই যথাসাধ্য চেষ্টা করি সঠিক সময়ে কিছু করার।",
+        'SecondViewCREATIVETextParagraph': "আমি একটি নতুন পদ্ধতি অনুসন্ধানকারী.",
+        'fontFamily': "'Noto Serif Bengali', serif",
+    },
+    // Arabic
+    'عَرَبِيّ': {
+        'MainIntroFirstViewIntroParagraph': "أنا عصامي ومدرب ذاتي ولا يقتصر على الدرجات العلمية أو التراخيص فقط. لقد تعلمت في كل ثانية وكل لحظة من حياتي أن أكون أفضل",
+        'SecondViewRESPONSIBLETextParagraph': "أنا مسؤول عما أفعله",
+        'SecondViewONTIMETextParagraph': "أبذل قصارى جهدي دائمًا للقيام بالأشياء في الوقت المناسب",
+        'SecondViewCREATIVETextParagraph': "أنا باحث عن طرق جديدة",
+        'fontFamily': "Amiri, serif",
+    },
+    // Persian
+    'فارسی': {
+        'MainIntroFirstViewIntroParagraph': "من یک خودآموخته و خودآموز هستم که خود را فقط با مدرک یا مجوز محدود نمی کند. من در هر ثانیه و هر لحظه از زندگی ام یاد گرفتم که خودم را بهتر کنم",
+        'SecondViewRESPONSIBLETextParagraph': "من مسئول کاری هستم که انجام می دهم",
+        'SecondViewONTIMETextParagraph': "من همیشه تمام تلاشم را می کنم که کارها را در زمان مناسب انجام دهم",
+        'SecondViewCREATIVETextParagraph': "من یک جستجوگر روش های جدید هستم",
+        'fontFamily': "'B Nazanin'",
     },
 };
 
@@ -60,6 +167,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Home',
         'DropDownSkillsTopNavBar': 'Skills',
         'MainPageFirstViewIntroHeading': "Hi! I'm Muhammad Husain Aboutalebi",
+        'SeeMySkillsButtonMainIntroFirstView': 'See My Skills',
+        'ScondViewRESPONSIBLECharactristics': 'Responsible',
+        'ScondViewONTIMECharactristics': 'On-Time',
+        'ScondViewCREATIVECharactristics': 'Creative',
         'fontFamily': "'Lilita One', serif",
     },
     // German
@@ -68,6 +179,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Heim',
         'DropDownSkillsTopNavBar': 'Fähigkeiten',
         'MainPageFirstViewIntroHeading': "Hallo! Ich bin Muhammad Husain Aboutalebi",
+        'SeeMySkillsButtonMainIntroFirstView': 'Siehe Meine Fähigkeiten',
+        'ScondViewRESPONSIBLECharactristics': 'Verantwortlich',
+        'ScondViewONTIMECharactristics': 'Pünktlich',
+        'ScondViewCREATIVECharactristics': 'Kreativ',
         'fontFamily': "'Lilita One', serif",
     },
     // Chinese (Simplified)
@@ -76,7 +191,11 @@ var staticTranslationsOfMyWebsite = {
         'Home': '家',
         'DropDownSkillsTopNavBar': '技能',
         'MainPageFirstViewIntroHeading': "你好！ 我是穆罕默德·侯赛因·阿布塔莱比",
-        'fontFamily': "'Ma Shan Zheng', serif",
+        'SeeMySkillsButtonMainIntroFirstView': '查看我的技能',
+        'ScondViewRESPONSIBLECharactristics': '负责任的',
+        'ScondViewONTIMECharactristics': '准时',
+        'ScondViewCREATIVECharactristics': '有创造力的',
+        'fontFamily': "'Ma Shan Zheng'",
     },
     // Russian
     'Русский': {
@@ -84,6 +203,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Дом',
         'DropDownSkillsTopNavBar': 'Навыки',
         'MainPageFirstViewIntroHeading': "Привет! Я Мухаммад Хусейн Обалеби.",
+        'SeeMySkillsButtonMainIntroFirstView': 'Посмотреть мои навыки',
+        'ScondViewRESPONSIBLECharactristics': 'Ответственный',
+        'ScondViewONTIMECharactristics': 'Вовремя',
+        'ScondViewCREATIVECharactristics': 'творческий',
         'fontFamily': "'Lilita One', serif",
     },
     // French
@@ -92,6 +215,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Maison',
         'DropDownSkillsTopNavBar': 'Compétences',
         'MainPageFirstViewIntroHeading': "Salut! Je m'appelle Muhammad Husain Aboutalebi",
+        'SeeMySkillsButtonMainIntroFirstView': 'Voir mes compétences',
+        'ScondViewRESPONSIBLECharactristics': 'Responsable',
+        'ScondViewONTIMECharactristics': 'À temps',
+        'ScondViewCREATIVECharactristics': 'Créatif',
         'fontFamily': "'Lilita One', serif",
     },
     // Japanese
@@ -100,6 +227,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': '家',
         'DropDownSkillsTopNavBar': 'スキル',
         'MainPageFirstViewIntroHeading': "こんにちは！ 私はムハマド・フサイン・アバウタレビです。",
+        'SeeMySkillsButtonMainIntroFirstView': '私のスキルを見る',
+        'ScondViewRESPONSIBLECharactristics': '責任者',
+        'ScondViewONTIMECharactristics': '時間通りに',
+        'ScondViewCREATIVECharactristics': 'クリエイティブ',
         'fontFamily': "'Dela Gothic One', serif",
     },
     // Korean
@@ -108,6 +239,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': '집',
         'DropDownSkillsTopNavBar': '기술',
         'MainPageFirstViewIntroHeading': "안녕! 저는 무하마드 후세인 아바탈레비입니다",
+        'SeeMySkillsButtonMainIntroFirstView': '내 기술 보기',
+        'ScondViewRESPONSIBLECharactristics': '책임이 있는',
+        'ScondViewONTIMECharactristics': '정시',
+        'ScondViewCREATIVECharactristics': '창의적인',
         'fontFamily': "'Black Han Sans', serif",
     },
     // Spanish
@@ -116,6 +251,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Hogar',
         'DropDownSkillsTopNavBar': 'Habilidades',
         'MainPageFirstViewIntroHeading': "¡Hola! Soy Muhammad Husain Aboutalebi.",
+        'SeeMySkillsButtonMainIntroFirstView': 'Ver mis habilidades',
+        'ScondViewRESPONSIBLECharactristics': 'Responsable',
+        'ScondViewONTIMECharactristics': 'A tiempo',
+        'ScondViewCREATIVECharactristics': 'Creativo',
         'fontFamily': "'Lilita One', serif",
     },
     // Hindi
@@ -124,6 +263,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'घर',
         'DropDownSkillsTopNavBar': 'कौशल',
         'MainPageFirstViewIntroHeading': "नमस्ते! मैं मुहम्मद हुसैन अबाउटलेबी हूं",
+        'SeeMySkillsButtonMainIntroFirstView': 'मेरा कौशल देखें',
+        'ScondViewRESPONSIBLECharactristics': 'जिम्मेदार',
+        'ScondViewONTIMECharactristics': 'समय पर',
+        'ScondViewCREATIVECharactristics': 'रचनात्मक',
         'fontFamily': "'Lilita One', serif",
     },
     // Portuguese
@@ -132,6 +275,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'Lar',
         'DropDownSkillsTopNavBar': 'Habilidades',
         'MainPageFirstViewIntroHeading': "Oi! Eu sou Muhammad Husain Aboutalebi",
+        'SeeMySkillsButtonMainIntroFirstView': 'Veja minhas habilidades',
+        'ScondViewRESPONSIBLECharactristics': 'Responsável',
+        'ScondViewONTIMECharactristics': 'Na hora',
+        'ScondViewCREATIVECharactristics': 'Criativo',
         'fontFamily': "'Lilita One', serif",
     },
     // Bengali
@@ -140,6 +287,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'বাড়ি',
         'DropDownSkillsTopNavBar': 'দক্ষতা',
         'MainPageFirstViewIntroHeading': "ওহে! আমি মুহাম্মদ হোসেন আবুতালেবী",
+        'SeeMySkillsButtonMainIntroFirstView': 'আমার দক্ষতা দেখুন',
+        'ScondViewRESPONSIBLECharactristics': 'দায়িত্বশীল',
+        'ScondViewONTIMECharactristics': 'অন-টাইম',
+        'ScondViewCREATIVECharactristics': 'সৃজনশীল',
         'fontFamily': "'Galada', serif",
     },
     // Arabic
@@ -148,6 +299,10 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'بيت',
         'DropDownSkillsTopNavBar': 'مهارات',
         'MainPageFirstViewIntroHeading': "أهلاً! انا محمد حسين ابوطالبي",
+        'SeeMySkillsButtonMainIntroFirstView': 'انظر مهاراتي',
+        'ScondViewRESPONSIBLECharactristics': 'مسؤول',
+        'ScondViewONTIMECharactristics': 'في الوقت المحدد',
+        'ScondViewCREATIVECharactristics': 'مبدع',
         'fontFamily': "'Lalezar', serif",
     },
     // Persian
@@ -156,7 +311,11 @@ var staticTranslationsOfMyWebsite = {
         'Home': 'خانه',
         'DropDownSkillsTopNavBar': 'توانایی ها',
         'MainPageFirstViewIntroHeading': "سلام! من محمد حسین ابوطالبی هستم",
-        'fontFamily': "'A Titraj 1'",
+        'SeeMySkillsButtonMainIntroFirstView': 'مهارت های من',
+        'ScondViewRESPONSIBLECharactristics': 'مسئولیت پذیر',
+        'ScondViewONTIMECharactristics': 'وقت شناس',
+        'ScondViewCREATIVECharactristics': 'خلاق',
+        'fontFamily': "'Lalezar', serif",
     },
 };
 
@@ -191,6 +350,42 @@ var neededProperties = {
         // Hi I'm Muhammad Husain Aboutalebi
         document.getElementById('first-view-intro-main-page-hi-im-mohammad-hussain-abootalebi').textContent = staticTranslationsOfMyWebsite[newLanguage]['MainPageFirstViewIntroHeading'];
         document.getElementById('first-view-intro-main-page-hi-im-mohammad-hussain-abootalebi').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // Contact Me in the Introduction
+        document.getElementById('first-view-main-intro-page-first-button-black').textContent = staticTranslationsOfMyWebsite[newLanguage]['ContactMe'];
+        document.getElementById('first-view-main-intro-page-first-button-black').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // See My Skills button
+        document.getElementById('see-my-skills-button-main-page-first-view-intro').textContent = staticTranslationsOfMyWebsite[newLanguage]['SeeMySkillsButtonMainIntroFirstView'];
+        document.getElementById('see-my-skills-button-main-page-first-view-intro').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // First view intro paragraph
+        document.getElementById('first-view-main-page-intro-paragraph-who-am-i').textContent = longTextParagraphsIntros[newLanguage]['MainIntroFirstViewIntroParagraph'];
+        document.getElementById('first-view-main-page-intro-paragraph-who-am-i').style.fontFamily = longTextParagraphsIntros[newLanguage]['fontFamily'];
+        
+        // Responsible Charactristics
+        document.getElementById('responsible-second-view-main-page-intro-charactristics').textContent = '  ' + staticTranslationsOfMyWebsite[newLanguage]['ScondViewRESPONSIBLECharactristics'];
+        document.getElementById('responsible-second-view-main-page-intro-charactristics').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // Responsible chractristics paragraph
+        document.querySelector('#small-text-responsible-second-view-charactristrics > span').textContent = '  ' + longTextParagraphsIntros[newLanguage]['SecondViewRESPONSIBLETextParagraph'];
+        document.querySelector('#small-text-responsible-second-view-charactristrics > span').style.fontFamily = longTextParagraphsIntros[newLanguage]['fontFamily'];
+
+        // On-Time charactristics
+        document.getElementById('on-time-second-view-main-page-intro-charactristics').textContent = '  ' + staticTranslationsOfMyWebsite[newLanguage]['ScondViewONTIMECharactristics'];
+        document.getElementById('on-time-second-view-main-page-intro-charactristics').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // On-Time charactristics Paragraph
+        document.querySelector('#small-text-on-time-second-view-charactristrics > span').textContent = '  ' + longTextParagraphsIntros[newLanguage]['SecondViewONTIMETextParagraph'];
+        document.querySelector('#small-text-on-time-second-view-charactristrics > span').style.fontFamily = longTextParagraphsIntros[newLanguage]['fontFamily'];
+
+        // Creative charactristics
+        document.getElementById('creative-second-view-main-page-intro-charactristics').textContent = '  ' + staticTranslationsOfMyWebsite[newLanguage]['ScondViewCREATIVECharactristics'];
+        document.getElementById('creative-second-view-main-page-intro-charactristics').style.fontFamily = staticTranslationsOfMyWebsite[newLanguage]['fontFamily'];
+        
+        // Creative charactristics Paragraph
+        document.querySelector('#small-text-creative-second-view-charactristrics > span').textContent = '  ' + longTextParagraphsIntros[newLanguage]['SecondViewCREATIVETextParagraph'];
+        document.querySelector('#small-text-creative-second-view-charactristrics > span').style.fontFamily = longTextParagraphsIntros[newLanguage]['fontFamily'];
         // ----------------------------↑ Translations ↑ ----------------------------
 
         // ---------------------------- ↓ Customization ↓ ----------------------------
